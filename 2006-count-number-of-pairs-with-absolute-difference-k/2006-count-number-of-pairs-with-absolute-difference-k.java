32207062c2c63d10cdcbee4b57a9397d188f3246
Pranjali  Punekar <94711940+Pranjali144@users.noreply.github.com>
2023-12-15 22:09:16 +0530

    Time: 4 ms (93.44%), Space: 43.2 MB (45.78%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -16,18 +16,13 @@ class Solution {
             int target1 = num + k;
 
             // Handle the case where k is equal to 0 separately
-            if (k == 0) {
-                // Count pairs where the frequency of an element is greater than or equal to 2
-                if (hasmap.get(num) >= 2) {
-                    count++;
-                }
-            } else {
+             
                 // Check if target1 exists in the hashmap
                 if (hasmap.containsKey(target1)) {
                     // If yes, increment the count by the product of frequencies
                     count += hasmap.get(num) * hasmap.get(target1);
                 }
-            }
+            
         }
 
         return count;
