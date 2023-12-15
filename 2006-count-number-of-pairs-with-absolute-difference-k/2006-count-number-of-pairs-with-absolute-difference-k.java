9785078a3a8e43508d6a1d227e24d6d1e752e486
Pranjali  Punekar <94711940+Pranjali144@users.noreply.github.com>
2023-12-15 21:56:13 +0530

    Time: 4 ms (93.44%), Space: 44 MB (5.82%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,25 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        
-        Map<Integer,Integer> hasmap = new HashMap<>();
-        
-        int count =0;
-        
-      
-        for(int i =0;i<nums.length;i++)
-        {
-          if(hasmap.containsKey(nums[i]+k))
-          {
-              count = count + hasmap.get(nums[i]+k);
-          }
-          if(hasmap.containsKey(nums[i]-k))
-          {
-             count = count + hasmap.get(nums[i]-k);
-          }
-            
-            hasmap.put(nums[i],hasmap.getOrDefault(nums[i],0)+1);
+        Map<Integer, Integer> hasmap = new HashMap<>();
+        int count = 0;
+
+        // Populate the hashmap with the frequency of each element
+        for (int num : nums) {
+            hasmap.put(num, hasmap.getOrDefault(num, 0) + 1);
         }
-        
+
+        // Iterate through the hashmap to count pairs with absolute difference k
+        for (int num : hasmap.keySet()) {
+            int target1 = num + k;
+
+            // Handle the case where k is equal to 0 separately
+            if (k == 0) {
+                // Count pairs where the frequency of an element is greater than or equal to 2
+                count += hasmap.get(num) >= 2 ? 1 : 0;
+            } else {
+                // Check if target1 exists in the hashmap
+                if (hasmap.containsKey(target1)) {
+                    // If yes, increment the count by the product of frequencies
+                    count += hasmap.get(num) * hasmap.get(target1);
+                }
+            }
+        }
+
         return count;
     }
 }
