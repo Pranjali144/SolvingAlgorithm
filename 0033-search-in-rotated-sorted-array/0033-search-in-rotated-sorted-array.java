01ccbd2e213e86d73ee3f6e43e81acd641dba9b6
Pranjali  Punekar <94711940+Pranjali144@users.noreply.github.com>
2023-12-22 18:35:55 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (5.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,35 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+        int start = 0;
+        int end =nums.length-1;
+        
+        while(start<=end)
+        {
+            int mid = (end-start)/2 + start;
+            if(nums[mid] == target)
+            {
                 return mid;
             }
-
-            // Check if the left half is sorted
-            if (nums[left] <= nums[mid]) {
-                // Check if the target is in the left half
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            
+            if(nums[start] <= nums[mid])
+            {
+                if(nums[start] <= target && target < nums[mid])
+                {
+                    end = mid -1;
                 }
-            } else {
-                // The right half is sorted
-                // Check if the target is in the right half
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                else
+                {
+                    start = mid+1;
                 }
             }
+            
+            else
+            {
+                
+             if(target > nums[mid] && target <= nums[end])   
+            {
+                start = mid +1;
+            }
+            else
+            {
+                end = mid -1;
+            }
+                
+            }
+            
+            
         }
-
-        return -1; // Target not found
+        
+        return -1;
     }
 }
