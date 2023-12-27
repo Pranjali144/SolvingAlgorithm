8548c26efd3182efc9a3b367743be877fe73bb35
Pranjali  Punekar <94711940+Pranjali144@users.noreply.github.com>
2023-12-27 14:19:58 +0530

    Time: 0 ms (100.00%), Space: 50.2 MB (37.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,24 +1,37 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        
         int start = 0;
-        int end = nums.length - 1;
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-
-            // Ensure mid is even
-            if (mid % 2 == 1) {
-                mid--;
+        int end = nums.length-1;
+        
+        if(nums.length == 1 || nums[0]!=nums[1])
+        {
+            return nums[start];
+        }
+        else if(nums[end]!=nums[end-1])
+        {
+            return nums[end];
+        }
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]!= nums[mid+1] && nums[mid] != nums[mid-1])
+            {
+                return nums[mid];
             }
-
-            // Check adjacent elements for the single element
-            if (nums[mid] == nums[mid + 1]) {
-                start = mid + 2;
-            } else {
-                end = mid;
+            
+            else if(mid %2 == 0 && nums[mid] == nums[mid+1] || mid %2 != 0 && nums[mid] == nums[mid-1])
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid -1;
             }
         }
-
-        return nums[start];
+        
+        return -1;
     }
 }
